b348e370c6c8f13eaa4bc845db352a3d714f7144
Edward Kim <ed@slickage.com>
2016-04-20 15:38:14 -1000

    Query board meta rather than lookup
    The board metadata was captured in the metadata.boards table but it just
    as fast to query most of that information on the fly instead of
    duplicating that data into another table. This addresses that issue. The
    only change not made was to the DB tables and trigger functions for now.

--- db/allCategories.js
+++ db/allCategories.js
@@ -13,7 +13,56 @@ module.exports = function(userPriority, opts) {
   .then(function(dbCategories) { categories = dbCategories; })
   // get all board mappings
   .then(function() {
-    var q = 'SELECT * FROM ( SELECT b.id, b.name, b.description, b.viewable_by, b.thread_count, b.post_count, b.created_at, b.updated_at, b.imported_at, mb.last_post_username, mb.last_post_created_at, mb.last_thread_id, mb.last_thread_title, mb.last_post_position, bm.parent_id, bm.category_id, bm.view_order FROM board_mapping bm LEFT JOIN boards b ON bm.board_id = b.id LEFT JOIN metadata.boards mb ON b.id = mb.board_id ) blist LEFT JOIN LATERAL ( SELECT p.deleted as post_deleted, u.id as user_id, u.deleted as user_deleted FROM posts p LEFT JOIN users u ON p.user_id = u.id WHERE blist.last_thread_id = p.thread_id ORDER BY p.created_at DESC LIMIT 1 ) p ON true LEFT JOIN LATERAL (SELECT json_agg(row_to_json((SELECT x FROM ( SELECT bm.user_id as id, u.username as username) x ))) as moderators from board_moderators bm LEFT JOIN users u ON bm.user_id = u.id WHERE bm.board_id = blist.id) mods on true';
+    var q = `SELECT * FROM (
+      SELECT
+        b.id,
+        b.name,
+        b.description,
+        b.viewable_by,
+        b.thread_count,
+        b.post_count,
+        b.created_at,
+        b.updated_at,
+        b.imported_at,
+        mb.last_thread_id,
+        bm.parent_id,
+        bm.category_id,
+        bm.view_order
+      FROM board_mapping bm
+      LEFT JOIN boards b ON bm.board_id = b.id
+      LEFT JOIN metadata.boards mb ON b.id = mb.board_id
+    ) blist
+    LEFT JOIN LATERAL (
+      SELECT pf.title as last_thread_title
+      FROM posts pf
+      WHERE pf.thread_id = blist.last_thread_id
+      ORDER BY pf.created_at
+      LIMIT 1
+    ) pfirst ON true
+    LEFT JOIN LATERAL (
+      SELECT
+        pl.deleted as post_deleted,
+        pl.created_at as last_post_created_at,
+        pl.position as last_post_position,
+        u.username as last_post_username,
+        u.id as user_id,
+        u.deleted as user_deleted
+      FROM posts pl
+      LEFT JOIN users u ON pl.user_id = u.id
+      WHERE blist.last_thread_id = pl.thread_id
+      ORDER BY pl.created_at DESC
+      LIMIT 1
+    ) plast ON true
+    LEFT JOIN LATERAL (
+      SELECT json_agg(
+        row_to_json(
+          (SELECT x FROM ( SELECT bm.user_id as id, u.username as username) x )
+        )
+      ) as moderators
+      FROM board_moderators bm
+      LEFT JOIN users u ON bm.user_id = u.id
+      WHERE bm.board_id = blist.id
+    ) mods on true;`;
     if (opts.stripped) {
       q = 'SELECT b.id, b.name, b.viewable_by, bm.parent_id, bm.category_id, bm.view_order FROM board_mapping bm LEFT JOIN boards b ON bm.board_id = b.id';
     }
