e60b43a1c48a4ab99a764d2ae5afdd1c1ed7d0a6
Edward Kim <ed@slickage.com>
2016-05-13 14:53:37 -1000

    Bug Fix: board write access scope
    Originally write access walked up the board parentage to check if any
    parent boards denined write access. This commit removes that feature.
    Now write access for boards is only limited to the board in question.

--- db/getBoardWriteAccess.js
+++ db/getBoardWriteAccess.js
@@ -1,49 +1,24 @@
 var path = require('path');
+var Promise = require('bluebird');
 var dbc = require(path.normalize(__dirname + '/db'));
 var db = dbc.db;
 var helper = dbc.helper;
+var NotFoundError = Promise.OperationalError;
 
 module.exports = function(boardId, userPriority) {
   boardId = helper.deslugify(boardId);
 
-  var q =
-  `WITH RECURSIVE find_parent(board_id, parent_id, category_id) AS (
-    SELECT bm.board_id, bm.parent_id, bm.category_id
-    FROM board_mapping bm where board_id = $1
-    UNION
-    SELECT bm.board_id, bm.parent_id, bm.category_id
-    FROM board_mapping bm, find_parent fp
-    WHERE bm.board_id = fp.parent_id
-  )
-  SELECT
-    fp.board_id,
-    fp.parent_id,
-    fp.category_id,
-    b.postable_by as board_postable,
-    c.postable_by as cat_postable
-  FROM find_parent fp
-  LEFT JOIN boards b on fp.board_id = b.id
-  LEFT JOIN categories c on fp.category_id = c.id`;
+  var q = `SELECT postable_by FROM boards WHERE id = $1`;
   return db.sqlQuery(q, [boardId])
   .then(function(rows) {
+    if (rows.length > 0 ) { return rows[0].postable_by; }
+    else { throw new NotFoundError(); }
+  })
+  .then(function(postable_by) {
     var postable = false;
-    if (rows.length < 1) { return postable; }
-
-    var boardPostable = true;
-    var catsPostable = true;
-    rows.forEach(function(row) {
-      var boardPriority = row.board_postable;
-      if (typeof boardPriority === 'number' && userPriority > boardPriority) {
-        boardPostable = false;
-      }
-
-      var catPriority = row.cat_postable;
-      if (typeof catPriority === 'number' && userPriority > catPriority) {
-        catsPostable = false;
-      }
-    });
-
-    if (boardPostable && catsPostable) { postable = true; }
+    if (postable_by === null || postable_by === undefined) { postable = true; }
+    else if (typeof postable_by === 'number' && userPriority <= postable_by) { postable = true; }
     return postable;
-  });
+  })
+  .error(function() { return false; });
 };
